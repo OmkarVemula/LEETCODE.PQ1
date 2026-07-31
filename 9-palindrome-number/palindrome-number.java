e1b791ee98a7d1f67f8a5e926eee32cc298d78a2
OmkarVemula <g542bcy9rd@privaterelay.appleid.com>
2026-07-31 19:51:01 +0530

    Time: 6 ms (22.37%) | Memory: 46.1 MB (33.98%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
         String s= String.valueOf(x);
         
         int k=s.length();
